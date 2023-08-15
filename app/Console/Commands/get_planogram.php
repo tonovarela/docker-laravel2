4f1c05667403310f94b68268af1b81d7eaad797d
Garrett O'Steen <garrett@prepango.com>
2023-08-15 13:17:03 -0700

    getting there

--- app/Console/Commands/get_planogram.php
+++ app/Console/Commands/get_planogram.php
@@ -2,7 +2,12 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+use App\Models\Machine_configuration;
+use App\Models\Planogram;
 
 class get_planogram extends Command
 {
@@ -25,6 +30,38 @@ class get_planogram extends Command
      */
     public function handle()
     {
-        //
+        $config = $this->get_config(); //Machine_configuration::all();
+        $uniqueRequestId = strtoupper(base_convert(Str::uuid(), 36, 30));
+        $machine_id = $config['machine_id'];
+        $user = $config['user'];
+        $pass = $config['pass'];
+
+        $req = array(
+            "request" => [
+                'uniqueRequestId' => $uniqueRequestId,
+                'authenticate' => [
+                    "username" => $user,
+                    "password" => $pass,
+                ],
+                'machine_id' => $machine_id,
+                
+            ],
+        );
+
+
+        //$response = Http::post('https://devapi.adminspend.com/api/v1/planogram', $req);
+        $response = Http::post('http://localhost:8000/api/v1/planogram', $req);
+
+    }
+
+
+    public function get_config()
+    {
+        $config = array();
+        foreach (Machine_configuration::all() as $cfg)
+        {
+            $config[$cfg->cfg_item] = $cfg->cfg_item_value;
+        }
+        return $config;
     }
 }
