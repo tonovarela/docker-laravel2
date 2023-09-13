8e49ffdc8dd3a3a738340df3817acf2dfd45d6e3
Garrett O'Steen <garrett@prepango.com>
2023-09-13 11:50:21 -0700

    detail view

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,78 +1,139 @@
+<style>
+  body {
+background: #eee;
+}
+.gallery {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	padding: 20px;
+	max-width: 90%;
+	-webkit-transform: translateX(-50%) translateY(-50%);
+	-moz-transform: translateX(-50%) translateY(-50%);
+	transform: translateX(-50%) translateY(-50%);
+	border: 1px solid #777;
+	overflow: hidden;
+}
+.gallery .inner {
+	position: relative;
+	overflow: hidden;
+	display: block;
+	width: auto;
+	max-width: 800px;
+}
+.gallery img {
+	display: none;
+}
+.main, .cd__main {
+       display: block !important;
+	position: relative;
+	width: 100%;
+	height: 500px;
+	overflow: hidden;
+	background: #ccc;
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-size: cover;
+	overflow: hidden;
+	margin-bottom: 20px;
+}
+.main:before {
+content: "";
+position: absolute;
+left: 0;
+bottom: 0;
+width: 100%;
+height: 100px;
+
+background: -moz-linear-gradient(top,  rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 100%);
+background: -webkit-linear-gradient(top,  rgba(0,0,0,0) 0%,rgba(0,0,0,0.7) 100%);
+background: linear-gradient(to bottom,  rgba(0,0,0,0) 0%,rgba(0,0,0,0.7) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', endColorstr='#a6000000',GradientType=0 );
+}
+.main-selected {
+	animation: crossfade 0.5s ease;
+	-webkit-animation: crossfade 0.5s ease;
+	-moz-animation: crossfade 0.5s ease;
+}
+@keyframes crossfade {
+	0% { opacity: 0.7; }
+	100% { opacity: 1; }
+}
+
+@-webkit-keyframes crossfade {
+	0% { opacity: 0.7; }
+	100% { opacity: 1; }
+}
+
+@-moz-keyframes crossfade {
+	0% { opacity: 0.7; }
+	100% { opacity: 1; }
+}
+.main span {
+	position: absolute;
+	display: block;
+	text-align: center;
+	font-size: 16px;
+	font-family: sans-serif;
+	color: #fff;
+	bottom: 10px;
+	left: 0;
+	right: 0;
+}
+.thumb-roll {
+position: relative;
+width: auto;
+overflow-x: auto;
+overflow-y: hidden;	
+
+white-space: nowrap;
+}
+.thumb {
+	display: inline-block;
+	position: relative;
+	width: 100px;
+	height: 100px;
+	margin-right: 20px;
+	background: #ccc;
+	overflow: hidden;
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-size: cover;
+	overflow: hidden;
+	cursor: pointer;
+}
+.thumb:last-of-type {
+	margin-right: 0px;
+}
+.thumb:after {
+	content: "";
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	box-shadow: inset 5px 5px 0px rgba(51, 204, 255, 0), inset -5px -5px 0px rgba(51, 204, 255, 0);
+}
+.thumb.current:after {
+	box-shadow: inset 5px 5px 0px rgba(51, 204, 255, 1), inset -5px -5px 0px rgba(51, 204, 255, 1);
+	background: rgba(255,255,255,0.4);
+	cursor: default;
+}
+.thumb:hover:after {
+	box-shadow: inset 5px 5px 0px rgba(51, 204, 255, 1), inset -5px -5px 0px rgba(51, 204, 255, 1);
+}
+</style>
 <x-app-layout>
-    @push('styles')
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/swiper/swiper-bundle.min.css"
-    />
-    @endpush
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ $product->name }}
-        </h2>
-    </x-slot>
-    <div class="mt-6 mx-60 swiper mySwiper">
-        <div class="swiper-wrapper">
-            
-          {{-- @if (isset($product->image1))
-          <div class="swiper-slide">
-            <img
-              class="object-cover w-full h-full"
-              src="{{ $product->image1 }}"
-              alt=""
-            />
-          </div>
-          @endif --}}
-          <div class="swiper-container" x-ref="container">
-            <div class="swiper-wrapper">
-              <!-- Slides -->
-              <div class="swiper-slide p-4">
-                <div class="flex flex-col rounded shadow overflow-hidden">
-                  <div class="flex-shrink-0">
-                    <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
-                  </div>
-                </div>
-              </div>
-                      
-              <div class="swiper-slide p-4">
-                <div class="flex flex-col rounded shadow overflow-hidden">
-                  <div class="flex-shrink-0">
-                    <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1598951092651-653c21f5d0b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="">
-                  </div>
-                </div>
-              </div>
-              
-              <div class="swiper-slide p-4">
-                <div class="flex flex-col rounded shadow overflow-hidden">
-                  <div class="flex-shrink-0">
-                    <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1598946423291-ce029c687a42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="">
-                  </div>
-                </div>
-              </div>
-        
+  <div class="container  py-8 mx-auto">
 
-
-        </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-pagination"></div>
-      </div>
-      @push('scripts')
-    <!-- Swiper JS -->
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script>
-      var swiper = new Swiper(".mySwiper", {
-        cssMode: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-        },
-        mousewheel: true,
-        keyboard: true,
-      });
-    </script>
-    @endpush
+<div class="gallery">
     
+  <img src="{{ $product->image1 }}" />
+  <img src="{{ $product->image2 }}" />
+  <img src="{{ $product->image3 }}" />
+
+
+
+</div>
+
+</div>
+<script src="/build/assets/gallery.js"></script>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         @stack('styles')
         <!-- Scripts -->
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
